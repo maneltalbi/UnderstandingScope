6ccd4193ac0a5066ee3f293fc0338bc7abb6e3b5
manel talbi <talbimanel20@gmail.com>
2022-01-08 15:03:50 +0100

    initial commit

---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UnderstandingScope
+{
+    class Program
+    {
+        private static string K = "";
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i =0; i<10; i++)
+            {
+                j = i.ToString();
+                K = i.ToString();
+                Console.WriteLine(i);
+
+            }
+            //Console.WriteLine(i);
+            Console.WriteLine("outside of the for: " + j);
+            Console.WriteLine("outside of the for: " + K);
+            HelperMethod();
+            Console.ReadLine();
+        }
+        static void HelperMethod()
+        {
+            Console.WriteLine("value of k from HelperMethod:" + K);
+        }
+
+    }
+}
