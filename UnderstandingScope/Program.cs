7069cbf8d3a10605523e0646964de6440ce0f986
manel talbi <talbimanel20@gmail.com>
2022-01-10 09:33:53 +0100

    final commit

--- UnderstandingScope/Program.cs
+++ UnderstandingScope/Program.cs
@@ -11,12 +11,20 @@
                 j = i.ToString();
                 K = i.ToString();
                 Console.WriteLine(i);
+                if (i==9)
+                {
+                    string l = i.ToString();
+                }
+               // Console.WriteLine(l);
 
             }
             //Console.WriteLine(i);
             Console.WriteLine("outside of the for: " + j);
             Console.WriteLine("outside of the for: " + K);
             HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
             Console.ReadLine();
         }
         static void HelperMethod()
@@ -24,5 +32,16 @@
             Console.WriteLine("value of k from HelperMethod:" + K);
         }
 
+    }
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+        private string helperMethod()
+        {
+            return "Hello world!";
+        }
     }
 }
